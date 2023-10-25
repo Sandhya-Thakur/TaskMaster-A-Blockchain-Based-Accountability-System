88c6ef82029a6f3d65221329f909c25b9c9a418d
Sandhya <thaku.sandhya@gmail.com>
2023-10-25 22:42:29 +0530

    first commit

---
+++ app/Examples/page.tsx
@@ -0,0 +1,11 @@
+export default function Examples(){
+    return(
+        <div>
+            <h1>
+                Example Button
+                
+            </h1>
+
+        </div>
+    )
+}
---
+++ app/NextPage/page.tsx
@@ -0,0 +1,10 @@
+export default function NextPage(){
+    return(
+        <div>
+            <h1>
+                This is the next page
+            </h1>
+
+        </div>
+    )
+}
